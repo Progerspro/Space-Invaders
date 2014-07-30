eb3bde1afd89b1d81f7e79fccbdcff2dc2ab2bf6
Progers <ru.progers@yandex.ru>
2014-07-30 21:59:44 +0400

    NewCommit!

---
+++ SpaceInvaders/SpaceInvaders.cpp
@@ -0,0 +1,14 @@
+// SpaceInvaders.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Starting Space Invaders!" << endl;
+
+	return 0;
+}
+
---
+++ SpaceInvaders/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+
+
+
+// TODO: reference additional headers your program requires here
